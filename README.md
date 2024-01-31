5c3d24e8056dbd8171f4e28c8da03e9699088237
yoptohlejepeta <peta.kotlan@seznam.cz>
2024-01-31 13:44:05 +0100

    data script

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - [Oborové skupiny číselník](https://vysledky.cermat.cz/data/PrehledOboruKKOV.aspx)
 - [Obory vzdělání](https://cs.wikipedia.org/wiki/Obor_vzd%C4%9Bl%C3%A1n%C3%AD)
 - [STAG ciselnik](https://ws.ujep.cz/ws/services/rest2/ciselniky/getCiselnik?domena=OBOR_SS&outputFormat=XLSX)
+- <https://www.infoabsolvent.cz/Obory/1#filtr-main>
+- <https://www.vyberskoly.cz/jak-se-vyznat-v-kodech-oboru>
 
 ## TODO
-
-- rozdil mezi gymnaziem a lyceem
---
+++ data.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import requests
+from io import StringIO
+
+stupen_vzdelani = {
+        "J": "Střední nebo střední odborné vzdělání (bez maturity i výučního listu)",
+        "C": "Praktická škola",
+        "E": "Střední odborné vzdělání s výučním listem",
+        "H": "Střední odborné vzdělání s výučním listem",
+        "M": "Úplné střední odborné vzdělání s maturitou (bez vyučení)",
+        "L": "Úplné střední odborné vzdělání s odborným výcvikem a maturitou",
+        "K": "Úplné střední všeobecné vzdělání (poskytují gymnázia)",
+        "N": "Vyšší odborné vzdělání",
+        "P": "Vyšší odborné vzdělání v konzervatoři",
+    }
+
+skupina_oboru = {
+        "16": "Ekologie a ochrana životního prostředí",
+        "18": "Informatické obory",
+        "21": "Hornictví a hornická geologie, hutnictví a slévárenství",
+        "23": "Strojírenství a strojírenská výroba",
+        "26": "Elektrotechnika, telekomunikační a výpočetní technika",
+        "28": "Technická chemie a chemie silikátů",
+        "29": "Potravinářství a potravinářská chemie",
+        "31": "Textilní výroba a oděvnictví",
+        "32": "Kožedělná a obuvnická výroba a zpracování plastů",
+        "33": "Zpracování dřeva a výroba hudebních nástrojů",
+        "34": "Polygrafie, zpracování papíru, filmu a fotografie",
+        "36": "Stavebnictví, geodézie a kartografie",
+        "37": "Doprava a spoje",
+        "39": "Speciální a interdisciplinární technické obory",
+        "41": "Zemědělství a lesnictví",
+        "43": "Veterinářství a veterinární prevence",
+        "53": "Zdravotnictví",
+        "61": "Filozofie, teologie",
+        "63": "Ekonomika a administrativa",
+        "64": "Podnikání v oborech, odvětví",
+        "65": "Gastronomie, hotelnictví a turismus",
+        "66": "Obchod",
+        "68": "Právo, právní a veřejnosprávní činnost",
+        "69": "Osobní a provozní služby",
+        "72": "Publicistika, knihovnictví a informatika",
+        "74": "Tělesná kultura, tělovýchova a sport",
+        "75": "Pedagogika, učitelství a sociální péče",
+        "78": "Obecně odborná příprava",
+        "79": "Obecná příprava",
+        "82": "Umění a užité umění",
+        "91": "Teorie vojenského umění",
+    }
+
+def transform_data():
+    df = pd.read_csv("data/prf_uchazeci.csv")
+    
+    df["KOLO_PRIHLASKY"] = df["PR_NAZEV"].str[-1].astype(int)
+    # ciselnik ze stagu
+    url = "https://ws.ujep.cz/ws/services/rest2/ciselniky/getCiselnik"
+
+    vars = {
+        "domena": "OBOR_SS",
+        "outputFormat": "CSV",
+        "outputFormatEncoding": "UTF-8",
+    }
+
+    response = requests.get(url, params=vars)
+    ciselnik = pd.read_csv(StringIO(response.text), sep=";")
+
+    df = df.merge(
+        ciselnik[["key", "nazev"]], left_on="CIS_OBORU", right_on="key")
+    df['skupina_oboru'] = df['CIS_OBORU'].str.extract(r'(\d{2})')
+    df['stupen_vzdelani'] = df['CIS_OBORU'].str.extract(r'([A-Za-z])')    
+    
+    df['skupina_oboru'].replace(skupina_oboru, inplace=True)
+    df['stupen_vzdelani'].replace(stupen_vzdelani, inplace=True)
+    
+    df["round"] = df["PR_NAZEV"].str[-1]
+    
+    return df
+
+
+if __name__=="__main__":
+    df = transform_data()
+    df.to_csv("uchazeci_transformed.csv")
