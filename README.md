a1b7a23e84ab8ff5017929c5edd71b5543a68c7d
yoptohlejepeta <peta.kotlan@seznam.cz>
2024-04-02 11:46:24 +0200

    okruhy done

--- README.md
+++ README.md
@@ -7,4 +7,3 @@
 - [STAG ciselnik](https://ws.ujep.cz/ws/services/rest2/ciselniky/getCiselnik?domena=OBOR_SS&outputFormat=XLSX)
 - <https://www.infoabsolvent.cz/Obory/1#filtr-main>
 - <https://www.vyberskoly.cz/jak-se-vyznat-v-kodech-oboru>
-
--- app/main.py
+++ app/main.py
@@ -15,7 +15,27 @@ st.title("Uchazeči UJEP PŘF")
 
 obor_tab, poloha_tab = st.tabs(["Obor", "Poloha"])
 
+# tabs obecny prehled (nejake filtrace)
+
 with obor_tab:
+    col1, col2 = st.columns(2)
+    okruhy_df = data[["OKRUH1", "OKRUH2"]].dropna()
+    pivot_table = okruhy_df.pivot_table(
+        index="OKRUH1", columns="OKRUH2", aggfunc=len, fill_value=0
+    )
+    okruh1_counts = okruhy_df["OKRUH1"].value_counts()
+    okruh2_counts = okruhy_df["OKRUH2"].value_counts()
+    
+    col1.table(pivot_table)
+    # col1.dataframe(pivot_table)
+    
+    fig_okruh1 = px.bar(okruh1_counts, x=okruh1_counts.index, y=okruh1_counts.values, title="OKRUH 1")
+    fig_okruh2 = px.bar(okruh2_counts, x=okruh2_counts.index, y=okruh2_counts.values, title="OKRUH 2")
+    
+    col2.plotly_chart(fig_okruh1)
+    col2.plotly_chart(fig_okruh2)
+    
+    
     obor = st.selectbox("Vyber obor", data["CZ_NAZEV"].unique())
 
     new_data = data[data["CZ_NAZEV"] == obor]
@@ -72,7 +92,11 @@ with poloha_tab:
             staty_bez_cr = data.loc[(data["STAT_OBCANSTVI"] != "Česká republika") & (data["ROK_PR"] == rok)]
             staty_counts = staty_bez_cr["STAT_OBCANSTVI"].value_counts()
             obory_counts = staty_bez_cr["PR_NAZEV"].value_counts()
+            
+            st.header("Počet uchazečů dle občanství (bez ČR)")
             fig = px.bar(staty_counts, x=staty_counts.index, y=staty_counts.values)
             st.plotly_chart(fig, use_container_width=True)
+            
+            st.header("Počet uchazečů dle oborů")
             fig = px.bar(obory_counts, x=obory_counts.index, y=obory_counts.values)
             st.plotly_chart(fig, use_container_width=True)
